915101378fcca5e4874ba28531bf1218a4104c4a
HarryPoint <1751826104@qq.com>
2017-08-25 16:21:02 +0800

    测试gitgutter

---
+++ index.js
@@ -0,0 +1,14 @@
+function  aadd () {
+  console.log('xxdfdsf')
+  console.log('sdfsdf')
+}
+- Sublime Text 3114
+- Platform: windows
+- Arch: x64
+- GitGutter 1.7.4
+- Install via PC: True
+- git version 2.11.1.windows.1
+- mdpopups not installed!
+- markdown not installed!
+- pygments not installed!
+- jinja2 2.8
